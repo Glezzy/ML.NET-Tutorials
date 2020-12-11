1a15675314efd61600c03a09b9d7fbea5a19c7b5
Glezzy <gsg@pdx.edu>
2020-12-11 09:12:40 -0700

    Sentiment Analysis Tutorial

---
+++ Sentiment-Analysis/myMLApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MyMLAppML.Model;
+
+namespace myMLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput();
+            Console.WriteLine("Type in a movie review to see if it is toxic or not.");
+            input.SentimentText = Convert.ToString(Console.ReadLine());
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}");
+        }
+    }
+}
