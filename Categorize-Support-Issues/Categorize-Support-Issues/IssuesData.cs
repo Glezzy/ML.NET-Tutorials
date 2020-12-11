390353ed19fb18e169170f56b71c5711761ecfa3
Glezzy <gsg@pdx.edu>
2020-12-11 13:12:54 -0700

    Building pipeline and model (still in progress)

---
+++ Categorize-Support-Issues/Categorize-Support-Issues/IssuesData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace Categorize_Support_Issues
+{
+
+    public class Issues   // This is our input dataset class and has 4 string fields 
+    {
+        [LoadColumn(0)]
+        public string ID { get; set; }
+        [LoadColumn(1)]
+        public string Area { get; set; }
+        [LoadColumn(2)]
+        public string Title { get; set; }
+        [LoadColumn(3)]
+        public string Description { get; set; }
+    }
+
+    // This is the class we will use for prediction after the model has been trained.  
+    // It has a single string(Area) and a "PredictLabel ColumnName" attribute.
+    // PredictLabel will be used during prediction and evaluation. 
+    public class IssuePrediction                                   
+    {
+        [ColumnName("PredictedLabel")]
+        public string Area;
+    }
+}
---
+++ Categorize-Support-Issues/Categorize-Support-Issues/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+
+namespace Categorize_Support_Issues
+{
+    class Program
+    {
+        private static string _appPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+
+        private static string _trainDataPath => Path.Combine(_appPath, "..", "..", "..", "Data", "issues_train.tsv");
+
+        private static string _testDataPath => Path.Combine(_appPath, "..", "..", "..", "Data", "issues_test.tsv");
+
+        private static string _modelPath => Path.Combine(_appPath, "..", "..", "..", "Models", "model.zip");
+
+        private static MLContext _mlContext;
+        private static PredictionEngine<Issues, IssuePrediction> _predEngine;
+        private static ITransformer _trainedModel;
+        static IDataView _trainingDataView;
+
+        // MLContext, when initialized, creates a new ML.NET environment that can be shared across the model creation workflow objects. Similar to DBContext in Entity Framework
+        static void Main(string[] args)
+        {
+            // We will initialize mlcontext with a random seed of 0. Similar to doing Data science in Python so we can get repeatable results. 
+            _mlContext = new MLContext(seed: 0);
+
+            Console.WriteLine($"=============== Loading Dataset  ===============");
+            // Initializing and loading _trainDataView as a global variable so it can be used in our pipeline.
+            _trainingDataView = _mlContext.Data.LoadFromTextFile<Issues>(_trainDataPath, hasHeader: true);
+            // Process data will extract and transform the data and then will return the processing pipeline.
+            var pipeline = ProcessData();
+        }
+
+        public static IEstimator<ITransformer> ProcessData()
+        {
+
+        }
+    }
+}
