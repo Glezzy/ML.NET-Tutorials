d92fe684327dfb6dfbc4e607d38710a1e1378971
Glezzy <gsg@pdx.edu>
2020-12-14 19:06:54 -0700

    Finished Categorize Support issues tutorial

--- Categorize-Support-Issues/Categorize-Support-Issues/Program.cs
+++ Categorize-Support-Issues/Categorize-Support-Issues/Program.cs
@@ -9,9 +9,9 @@ namespace Categorize_Support_Issues
     {
         private static string _appPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
 
-        private static string _trainDataPath => Path.Combine(_appPath, "..", "..", "..", "Data", "issues_train.tsv");
+        private static string _trainDataPath => Path.Combine(_appPath, "Data", "issues_train.tsv");
 
-        private static string _testDataPath => Path.Combine(_appPath, "..", "..", "..", "Data", "issues_test.tsv");
+        private static string _testDataPath => Path.Combine(_appPath, "Data", "issues_test.tsv");
 
         private static string _modelPath => Path.Combine(_appPath, "..", "..", "..", "Models", "model.zip");
 
@@ -31,8 +31,9 @@ namespace Categorize_Support_Issues
             _trainingDataView = _mlContext.Data.LoadFromTextFile<Issues>(_trainDataPath, hasHeader: true);
             // Process data will extract and transform the data and then will return the processing pipeline.
             var pipeline = ProcessData();
+            // BuildAndTrainModel method creates the training algorithm class, trains the model, predicts area based on training data and eturnds the model. 
+            var trainingPipeline = BuildAndTrainModel(_trainingDataView, pipeline);
         }
-
         public static IEstimator<ITransformer> ProcessData()
         {
             Console.WriteLine($"=============== Processing Data ===============");
@@ -46,7 +47,116 @@ namespace Categorize_Support_Issues
                             // Lastly we shall combine all the feature columns into the Features column using Concatenate()
                             .Append(_mlContext.Transforms.Concatenate("Features", "TitleFeaturized", "DescriptionFeaturized"))
 
+                            .AppendCacheCheckpoint(_mlContext);
 
+            return pipeline;
+        }
+        public static IEstimator<ITransformer> BuildAndTrainModel(IDataView trainingDataView, IEstimator<ITransformer> pipeline)
+        {
+            //The SdcaMaximumEntropy is your multiclass classification training algorithm. 
+            // This is appended to the pipeline and accepts the featurized Title and Description (Features) and the Label input parameters to learn from the historic data.
+            var trainingPipeline = pipeline.Append(_mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy("Label", "Features"))
+                .Append(_mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+            Console.WriteLine($"=============== Training the model  ===============");
+
+            
+            _trainedModel = trainingPipeline.Fit(trainingDataView);
+            
+            Console.WriteLine($"=============== Finished Training the model Ending time: {DateTime.Now.ToString()} ===============");
+
+            // (OPTIONAL) Try/test a single prediction with the "just-trained model" (Before saving the model)
+            Console.WriteLine($"=============== Single Prediction just-trained-model ===============");
+
+            // Create prediction engine related to the loaded trained model
+            // <SnippetCreatePredictionEngine1>
+            _predEngine = _mlContext.Model.CreatePredictionEngine<Issues, IssuePrediction>(_trainedModel);
+            // </SnippetCreatePredictionEngine1>
+            // <SnippetCreateTestIssue1>
+            Issues issue = new Issues()
+            {
+                Title = "WebSockets communication is slow in my machine",
+                Description = "The WebSockets communication used under the covers by SignalR looks like is going slow in my development machine.."
+            };
+            // </SnippetCreateTestIssue1>
+
+            // <SnippetPredict>
+            var prediction = _predEngine.Predict(issue);
+            // </SnippetPredict>
+
+            // <SnippetOutputPrediction>
+            Console.WriteLine($"=============== Single Prediction just-trained-model - Result: {prediction.Area} ===============");
+            // </SnippetOutputPrediction>
+
+            // <SnippetReturnModel>
+            return trainingPipeline;
+            // </SnippetReturnModel>
+        }
+
+        public static void Evaluate(DataViewSchema trainingDataViewSchema)
+        {
+            // STEP 5:  Evaluate the model in order to get the model's accuracy metrics
+            Console.WriteLine($"=============== Evaluating to get model's accuracy metrics - Starting time: {DateTime.Now.ToString()} ===============");
+
+            //Load the test dataset into the IDataView
+            // <SnippetLoadTestDataset>
+            var testDataView = _mlContext.Data.LoadFromTextFile<Issues>(_testDataPath, hasHeader: true);
+            // </SnippetLoadTestDataset>
+
+            //Evaluate the model on a test dataset and calculate metrics of the model on the test data.
+            // <SnippetEvaluate>
+            var testMetrics = _mlContext.MulticlassClassification.Evaluate(_trainedModel.Transform(testDataView));
+            // </SnippetEvaluate>
+
+            Console.WriteLine($"=============== Evaluating to get model's accuracy metrics - Ending time: {DateTime.Now.ToString()} ===============");
+            // <SnippetDisplayMetrics>
+            Console.WriteLine($"*************************************************************************************************************");
+            Console.WriteLine($"*       Metrics for Multi-class Classification model - Test Data     ");
+            Console.WriteLine($"*------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine($"*       MicroAccuracy:    {testMetrics.MicroAccuracy:0.###}");
+            Console.WriteLine($"*       MacroAccuracy:    {testMetrics.MacroAccuracy:0.###}");
+            Console.WriteLine($"*       LogLoss:          {testMetrics.LogLoss:#.###}");
+            Console.WriteLine($"*       LogLossReduction: {testMetrics.LogLossReduction:#.###}");
+            Console.WriteLine($"*************************************************************************************************************");
+            // </SnippetDisplayMetrics>
+
+            // Save the new model to .ZIP file
+            // <SnippetCallSaveModel>
+            SaveModelAsFile(_mlContext, trainingDataViewSchema, _trainedModel);
+            // </SnippetCallSaveModel>
+        }
+
+        public static void PredictIssue()
+        {
+            // <SnippetLoadModel>
+            ITransformer loadedModel = _mlContext.Model.Load(_modelPath, out var modelInputSchema);
+            // </SnippetLoadModel>
+
+            // <SnippetAddTestIssue>
+            Issues singleIssue = new Issues() { Title = "Entity Framework crashes", Description = "When connecting to the database, EF is crashing" };
+            // </SnippetAddTestIssue>
+
+            //Predict label for single hard-coded issue
+            // <SnippetCreatePredictionEngine>
+            _predEngine = _mlContext.Model.CreatePredictionEngine<Issues, IssuePrediction>(loadedModel);
+            // </SnippetCreatePredictionEngine>
+
+            // <SnippetPredictIssue>
+            var prediction = _predEngine.Predict(singleIssue);
+            // </SnippetPredictIssue>
+
+            // <SnippetDisplayResults>
+            Console.WriteLine($"=============== Single Prediction - Result: {prediction.Area} ===============");
+            // </SnippetDisplayResults>
+        }
+
+        private static void SaveModelAsFile(MLContext mlContext, DataViewSchema trainingDataViewSchema, ITransformer model)
+        {
+            // <SnippetSaveModel>
+            mlContext.Model.Save(model, trainingDataViewSchema, _modelPath);
+            // </SnippetSaveModel>
+
+            Console.WriteLine("The model is saved to {0}", _modelPath);
         }
     }
 }
